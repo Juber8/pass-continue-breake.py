36bff8d72b5cd3debd8788b10a3f39468c41cb4b
Juber8 <90597119+Juber8@users.noreply.github.com>
2021-11-09 16:19:00 +0530

    transfer statement in python

---
+++ break continue pass .py
@@ -0,0 +1,20 @@
+'''
+# Break Statement
+
+for i in range(1,11):
+    if i == 5:
+        break
+    print(i)
+    # Continue Statement
+
+for i in range(1, 11):
+        if i == 5:
+            continue
+        print(i)
+        print("Hello")
+'''
+# Pass Statement
+
+for i in range(1,11):
+    pass
+ 
